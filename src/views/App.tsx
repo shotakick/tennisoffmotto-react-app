abb86646829bea4060f4b4f09b94bcc093e24243
shotakick <chrysanth.pool@gmail.com>
2019-01-16 18:44:36 +0900

    Add title bar

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,25 +1,21 @@
 import * as React from 'react';
-import { Header } from 'semantic-ui-react';
 import './App.css';
-import EventList from './containers/EventList';
+import MainMenu from './components/MainMenu';
 import EventMap from './containers/EventMap';
-import FetchEventsButton from './containers/FetchEventsButton';
-import FilterEventsButton from './containers/FilterEventsButton';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App" style={{ height: '100vh' }}>
-        <Header as="h1">テストアプリ!</Header>
-        <FetchEventsButton content="イベント取得" />
-        <FilterEventsButton content="男性のみ" filter={{ sex: '男性' }} />
-        <FilterEventsButton
-          // className="ui disabled"
-          content="女性のみ"
-          filter={{ sex: '女性' }}
-        />
-        <EventMap maxMarkerVisibleCount={100} />
-        <EventList header="イベントリスト" />
+      <div className="App">
+        <MainMenu />
+        <div className="Title">
+          <img className="Title-logo" src="images/tennis-ball.png" />
+          <span className="Title-text">Tennisoff.motto</span>
+        </div>
+        <div className="App-content">
+          <EventMap maxMarkerVisibleCount={100} />
+        </div>
+        <footer />
       </div>
     );
   }
---
+++ src/views/components/MainMenu.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { Dropdown, Menu, MenuProps } from 'semantic-ui-react';
+import FetchEventsButton from 'views/containers/FetchEventsButton';
+import FilterEventsButton from 'views/containers/FilterEventsButton';
+
+const menuProps: MenuProps = {
+  color: 'teal',
+  inverted: true,
+  stackable: true
+};
+
+const mainMenu: React.FC = () => (
+  <Menu attached="top" {...menuProps}>
+    <Dropdown item={true} icon="content" simple={true}>
+      <Dropdown.Menu>
+        <Dropdown.Item>
+          <FetchEventsButton content="イベント取得" />
+        </Dropdown.Item>
+        <Dropdown.Divider />
+        <Dropdown.Header>フィルタ</Dropdown.Header>
+        <Dropdown.Item>
+          <FilterEventsButton content="男性のみ" filter={{ sex: '男性' }} />
+        </Dropdown.Item>
+        <Dropdown.Item>
+          <FilterEventsButton content="女性のみ" filter={{ sex: '女性' }} />
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    <Menu.Menu position="right" {...menuProps}>
+      <div className="ui right aligned category search item">
+        <div className="ui transparent icon input">
+          <input className="prompt" type="text" placeholder="Search place..." />
+          <i className="search link icon" />
+        </div>
+        <div className="results" />
+      </div>
+    </Menu.Menu>
+  </Menu>
+);
+export default mainMenu;
