701bc6178ccb8c30d380d78f4aa085398f03fea2
shotakick <chrysanth.pool@gmail.com>
2019-01-17 01:34:31 +0900

    Implement EventInfoWindow

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './TypeUtils';
 export * from './ObjectUtils';
+export * from './DateUtils';
--- src/views/components/EventInfoWindow.tsx
+++ src/views/components/EventInfoWindow.tsx
@@ -1,24 +1,70 @@
 import { TennisEventInfo } from 'client/TennisEvents';
+import * as Moment from 'moment';
+import 'moment/locale/ja';
 import * as React from 'react';
 import { InfoWindow, InfoWindowProps } from 'react-google-maps';
+import { Divider, List } from 'semantic-ui-react';
+
+const INFO_URL = 'https://www.tennisoff.net/off';
+const USER_URL = 'https://www.tennisoff.net/profile';
+
+const getTitle = (event: TennisEventInfo) => (
+  <a href={`${INFO_URL}/${event.sid}`} target={'_blank'} rel={'noopener'}>
+    {`${event.title} (${event.sid})`}
+  </a>
+);
+const getOrganizer = (event: TennisEventInfo) => (
+  <a href={`${USER_URL}/${event.organizer}`} target={'_blank'} rel={'noopener'}>
+    {`${event.organizer} (${event.sex})`}
+  </a>
+);
+const getDate = (event: TennisEventInfo) =>
+  Moment(event.date)
+    .locale('ja')
+    .format(`M月D日(ddd) k時mm分 から ${event.timespan}時間`);
+const getInfo = (event: TennisEventInfo) =>
+  `${event.capacity}人 / ${event.courtType}(${event.courtCount}面)`;
 
 type EventInfoWindowProps = {
   events: TennisEventInfo[];
 } & InfoWindowProps;
 
-export default function eventInfoWindow({
+const eventInfoWindow: React.FC<EventInfoWindowProps> = ({
   events,
   ...infoWindowProps
-}: EventInfoWindowProps) {
-  return (
-    <InfoWindow {...infoWindowProps}>
-      <span>Something</span>
-      {/* {events.map(event => (
+}) => (
+  <InfoWindow {...infoWindowProps}>
+    <div>
+      {events.map((event, index) => (
         <div key={event.sid}>
-          <h1>{event.title}</h1>
-          <p>{`${event.organizer}(${event.sex})`}</p>
+          <List>
+            <List.Item>
+              <List.Icon name="linkify" />
+              <List.Content>
+                <List.Header>{getTitle(event)}</List.Header>
+              </List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name="user" />
+              <List.Content>{getOrganizer(event)}</List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name="calendar alternate outline" />
+              <List.Content>{getDate(event)}</List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name="map marker alternate" />
+              <List.Content>{event.placeName}</List.Content>
+            </List.Item>
+            <List.Item>
+              <List.Icon name="clipboard outline" />
+              <List.Content>{getInfo(event)}</List.Content>
+            </List.Item>
+          </List>
+          {index + 1 < events.length && <Divider fitted={true} />}
         </div>
-      ))} */}
-    </InfoWindow>
-  );
-}
+      ))}
+    </div>
+  </InfoWindow>
+);
+export default eventInfoWindow;
