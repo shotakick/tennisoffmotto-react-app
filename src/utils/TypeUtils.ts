a365b8d9302c639db531db3d1377e2039b83f907
shotakick <chrysanth.pool@gmail.com>
2019-04-03 16:40:34 +0900

    Add RequireOne(), PartialRequire()

--- src/utils/TypeUtils.ts
+++ src/utils/TypeUtils.ts
@@ -2,3 +2,7 @@ export type Diff<T, U> = T extends U ? never : T;
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 export type Wrap<Org, Over> = Omit<Org, Extract<keyof Org, keyof Over>> & Over;
 export type Mapper<Inner, Outer> = (input: Inner) => Outer;
+export type RequireOne<T, K extends keyof T = keyof T> = K extends keyof T
+  ? PartialRequire<T, K>
+  : never;
+export type PartialRequire<O, K extends keyof O> = { [P in K]-?: O[P] } & O;
