681db4c075121eb56ad1097626529fcd4b48e06c
shotakick <chrysanth.pool@gmail.com>
2019-04-11 16:59:50 +0900

    Change initial location to present

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,25 +1,58 @@
 import * as React from 'react';
+import { GoogleMapProps } from 'react-google-maps';
 import './App.css';
 import MainMenu from './components/MainMenu';
 import EventMap from './containers/EventMap';
 import { initialMapOptions } from './ini/EventMap';
 
-class App extends React.Component {
+interface AppState {
+  initialMapOptions: GoogleMapProps;
+  maxMarkerVisibleCount: number;
+}
+
+class App extends React.Component<{}, AppState> {
+  public constructor(props: {}) {
+    super(props);
+    this.state = { initialMapOptions, maxMarkerVisibleCount: 300 };
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.setCenterPosition);
+    }
+  }
+
   public render() {
     return (
       <div className="App">
-        <MainMenu />
-        <div className="Title">
-          <img className="Title-logo" src="images/tennis-ball.png" />
-          <span className="Title-text">Tennisoff.motto</span>
-        </div>
+        <header>
+          <MainMenu />
+          <div className="Title">
+            <img className="Title-logo" src="images/tennis-ball.png" />
+            <span className="Title-text">Tennisoff.motto</span>
+          </div>
+        </header>
+
         <div className="App-content">
-          <EventMap maxMarkerVisibleCount={300} {...initialMapOptions} />
+          <EventMap
+            {...this.state.initialMapOptions}
+            maxMarkerVisibleCount={this.state.maxMarkerVisibleCount}
+          />
         </div>
         <footer />
       </div>
     );
   }
+
+  private setCenterPosition = (pos: Position) => {
+    this.setState({
+      initialMapOptions: {
+        ...this.state.initialMapOptions,
+        defaultCenter: {
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude
+        }
+      }
+    });
+  };
 }
 
 export default App;
