96fbcedc6f4a6ba3bf3f696ef4908c338a288a57
shotakick <chrysanth.pool@gmail.com>
2019-04-12 20:07:30 +0900

    Remove unused props passing over to wrapped container

--- src/views/containers/FetchEventsButton.ts
+++ src/views/containers/FetchEventsButton.ts
@@ -1,32 +1,28 @@
-import { connect } from 'react-redux';
-import { bindActionCreators, Dispatch } from 'redux';
+import { connect, DispatchProp } from 'react-redux';
+import { Dispatch } from 'redux';
 import { Button, ButtonProps } from 'semantic-ui-react';
 import { ReduxRootState } from 'state/ducks';
 import { actionCreators } from 'state/ducks/TennisEvents';
 import { Action } from 'typescript-fsa';
 
-function mapStateToProps(state: ReduxRootState) {
-  return {
-    previousFetchingParams: state.tennisEvents.fetchingParams
-  };
-}
-
-function mapDispatchToProps(
-  dispatch: Dispatch<Action<any>>,
-  { previousFetchingParams }: StateProps
-) {
-  return bindActionCreators(
-    {
-      onClick: () =>
-        actionCreators.requestFetchTennisEvents(previousFetchingParams)
-    },
-    dispatch
-  );
-}
-
 type StateProps = ReturnType<typeof mapStateToProps>;
+const mapStateToProps = (state: ReduxRootState) => ({
+  previousFetchingParams: state.tennisEvents.fetchingParams
+});
+
 type DispatchingProps = ReturnType<typeof mapDispatchToProps>;
+const mapDispatchToProps = (dispatch: Dispatch<Action<any>>) => ({ dispatch });
+
+const mergeProps = (
+  { previousFetchingParams }: StateProps,
+  { dispatch }: DispatchProp
+) => ({
+  onClick: () =>
+    dispatch(actionCreators.requestFetchTennisEvents(previousFetchingParams))
+});
+
 export default connect<StateProps, DispatchingProps, ButtonProps>(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
+  mergeProps
 )(Button);
